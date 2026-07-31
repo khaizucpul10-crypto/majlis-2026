df680405adbc8eefa5d86de48eedd2d1b2e1a835
khaizucpul10-crypto <khaizucpul10@gmail.com>
2026-07-31 15:41:00 +0800

    Add admin user and password to config.js
    Added admin user credentials to the configuration file.

--- config.js
+++ config.js
@@ -1,1 +1,3 @@
 const API_URL = "https://script.google.com/macros/s/AKfycbxiINgxZ5BFAWwX5DJ_2kDQyPWQ8Ou9GzwQEvzDjZafV96GdUhJop4ikBWHj6iaqHBW/exec";
+const ADMIN_USER = "admin";
+const ADMIN_PASS = "ppdtawau2026";
